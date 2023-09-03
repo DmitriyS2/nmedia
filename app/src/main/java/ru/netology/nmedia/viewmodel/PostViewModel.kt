82ddce238853033e996159cdeb4014e4fd3cc2a1
Dmitriy <dim116@inbox.ru>
2023-09-03 18:10:39 +0300

    added actionPOST

--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -1,12 +1,16 @@
 package ru.netology.nmedia.viewmodel
 
 import android.app.Application
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import ru.netology.nmedia.db.AppDb
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.repository.PostRepository
 import ru.netology.nmedia.repository.PostRepositoryRoomImpl
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 
 private val empty = Post(
@@ -32,11 +36,16 @@ class PostViewModel(application: Application) : AndroidViewModel(application) {
 
     val edited = MutableLiveData(empty)
 
+    @RequiresApi(Build.VERSION_CODES.O)
     fun changeContentAndSave(content: String) {
+
+        val dateTime = LocalDateTime.now()
+            .format(DateTimeFormatter.ofPattern("dd MMMM yyyy, hh:mm a"))
+
         edited.value?.let {
             val text = content.trim()
             if (it.content != text) {
-                repository.save(it.copy(content = text))
+                repository.save(it.copy(content = text, published = dateTime))
             }
         }
         edited.value = empty
