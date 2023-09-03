7c6c4e608344d7b6d80a1ee11ea5768a3090c35f
Dmitriy <dim116@inbox.ru>
2023-09-03 14:01:24 +0300

    push, notification

--- app/src/main/java/ru/netology/nmedia/repository/PostRepositorySQLiteImpl.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositorySQLiteImpl.kt
@@ -1,38 +1,43 @@
 package ru.netology.nmedia.repository
 
-//class PostRepositorySQLiteImpl(
-//    private val dao: PostDao
-//) : PostRepository {
-//    private var posts = emptyList<Post>()
-//    private val data = MutableLiveData(posts)
-//
-//    init {
-//        posts = dao.getAll()
-//        data.value = posts
-//    }
-//
-//    override fun getAll(): LiveData<List<Post>> = data
-//
-//    override fun like(id: Long) {
-//        dao.likeById(id)
-//        posts = posts.map {
-//            if (it.id != id) it else it.copy(
-//                likedByMe = !it.likedByMe,
-//                likes = if (it.likedByMe) it.likes - 1 else it.likes + 1
-//            )
-//        }
-//        data.value = posts
-//    }
-//
-//    override fun share(id: Long) {
-//        dao.shareById(id)
-//        posts = posts.map {
-//            if (it.id != id) it else it.copy(shares = (it.shares + 1))
-//        }
-//        data.value = posts
-//    }
-//
-//    override fun save(post: Post) {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import ru.netology.nmedia.dao.PostDao
+import ru.netology.nmedia.dto.Post
+
+class PostRepositorySQLiteImpl(
+    private val dao: PostDao
+) : PostRepository {
+    private var posts = emptyList<Post>()
+    private val data = MutableLiveData(posts)
+
+    init {
+        //posts = dao.getAll()
+        data.value = posts
+    }
+
+    override fun getAll(): LiveData<List<Post>> = data
+
+    override fun likeById(id: Long) {
+        dao.likeById(id)
+        posts = posts.map {
+            if (it.id != id) it else it.copy(
+                likedByMe = !it.likedByMe,
+                likes = if (it.likedByMe) it.likes - 1 else it.likes + 1
+            )
+        }
+        data.value = posts
+    }
+
+    override fun shareById(id: Long) {
+        dao.shareById(id)
+        posts = posts.map {
+            if (it.id != id) it else it.copy(shares = (it.shares + 1))
+        }
+        data.value = posts
+    }
+
+    override fun save(post: Post) {
 //        val id = post.id
 //        val saved = dao.save(post)
 //        posts = if (id == 0L) {
@@ -43,11 +48,11 @@ package ru.netology.nmedia.repository
 //            }
 //        }
 //        data.value = posts
-//    }
-//
-//    override fun removeById(id: Long) {
-//        dao.removeById(id)
-//        posts = posts.filter { it.id != id }
-//        data.value = posts
-//    }
-//}
+    }
+
+    override fun removeById(id: Long) {
+        dao.removeById(id)
+        posts = posts.filter { it.id != id }
+        data.value = posts
+    }
+}
