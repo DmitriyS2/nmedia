6f985d34ea2b4a5e117bce2995dcd6f54d33d105
Dmitriy <dim116@inbox.ru>
2023-11-18 09:16:48 +0300

    added observe for post in currentPhotoFragment

--- app/src/main/java/ru/netology/nmedia/activity/CurrentPhotoFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/CurrentPhotoFragment.kt
@@ -13,6 +13,7 @@ import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import ru.netology.nmedia.databinding.FragmentCurrentPhotoBinding
 import ru.netology.nmedia.dto.CounterView
+import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.util.StringArg
 import ru.netology.nmedia.view.load
 import ru.netology.nmedia.viewmodel.PostViewModel
@@ -24,7 +25,6 @@ class CurrentPhotoFragment : Fragment() {
     }
 
     private val viewModel: PostViewModel by activityViewModels()
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,25 +38,45 @@ class CurrentPhotoFragment : Fragment() {
 
         val currentId = arguments?.textArgument?.toLong()
 
-        val currentPost = viewModel.data.value?.posts
-            ?.first { it.id == currentId }
+        viewModel.data.observe(viewLifecycleOwner) { list ->
+            list.posts.find { it.id == currentId }?.let { currentPost ->
 
-        if (currentPost != null) {
-            binding.apply {
-                currentPhoto.load("http://192.168.1.10:9999/media/${currentPost.attachment?.url}")
+                binding.apply {
+                    currentPhoto.load("http://192.168.1.10:9999/media/${currentPost.attachment?.url}")
 
-                likePhoto.isChecked = currentPost.likedByMe
-                likePhoto.text = CounterView.createCount(currentPost.likes)
+                    likePhoto.isChecked = currentPost.likedByMe
+                    likePhoto.text = CounterView.createCount(currentPost.likes)
 
-                likePhoto.setOnClickListener {
-                    viewModel.likeById(currentPost)
-                }
+                    likePhoto.setOnClickListener {
+                        viewModel.likeById(currentPost)
+                    }
 
-                buttonReturn.setOnClickListener {
-                    findNavController().navigateUp()
+                    buttonReturn.setOnClickListener {
+                        findNavController().navigateUp()
+                    }
                 }
             }
         }
+
+//        val currentPost = viewModel.data.value?.posts
+//            ?.first { it.id == currentId }
+//
+//        if (currentPost != null) {
+//            binding.apply {
+//                currentPhoto.load("http://192.168.1.10:9999/media/${currentPost.attachment?.url}")
+//
+//                likePhoto.isChecked = currentPost.likedByMe
+//                likePhoto.text = CounterView.createCount(currentPost.likes)
+//
+//                likePhoto.setOnClickListener {
+//                    viewModel.likeById(currentPost)
+//                }
+//
+//                buttonReturn.setOnClickListener {
+//                    findNavController().navigateUp()
+//                }
+//            }
+//        }
 
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
             findNavController().navigateUp()
